e86c92a0b13aae1766e74ee421b1a0a1359924f6
kalyan joshi <139383788+joshi009@users.noreply.github.com>
2024-03-10 19:10:48 +0530

    Update Area.java

--- src/variable/Area.java
+++ src/variable/Area.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Area {
     public static void main(String args [] ) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the radius");
+        System.out.println("Enter the radius first ");
         float rad = sc.nextFloat();
 //        float area = 3.14f * rad * rad;
         float area = 3.14f * rad * rad;
