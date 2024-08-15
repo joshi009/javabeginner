8dee88c7cc81c25c5e5bd24a835669eb67d87392
kalyan joshi <joshi1307kashyap@gmail.com>
2024-08-15 23:03:19 +0530

    java

--- src/Array/PairsArray.java
+++ src/Array/PairsArray.java
@@ -3,13 +3,16 @@ import java.util.*;
 
 public class PairsArray {
     public static void Pairs(int numbers[] ){
+        int tp=0;
         for(int i=0; i<numbers.length; i++){
             int curr=numbers[i];
             for(int j=i+1; j<numbers.length; j++){
                 System.out.print("(" + curr + "," + numbers[j]+ ")");
+                tp++;
             }
             System.out.println();
         }
+        System.out.println("total pairs = " + tp);
     }
 
     public static void main(String args[]) {
