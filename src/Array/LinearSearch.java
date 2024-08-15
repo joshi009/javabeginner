4634ad7d5d4e5ceb05df0180b8b6aec298ebb97d
kalyan joshi <joshi1307kashyap@gmail.com>
2024-08-15 21:30:43 +0530

    java

--- src/Array/LinearSearch.java
+++ src/Array/LinearSearch.java
@@ -11,7 +11,7 @@ public class LinearSearch {
     }
     public static void main(String[] args) {
         int[] numbers = {2,4,6,8,10,12,14,18};
-        int key = 10;
+        int key = 14;
 
         int index = linearSearch(numbers,key);
         if(index == -1){
