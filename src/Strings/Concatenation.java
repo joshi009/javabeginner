6d710a5df65375225085a3b428c2a44853a3e327
kalyan joshi <joshi1307kashyap@gmail.com>
2024-09-09 21:47:10 +0530

    java

--- src/Strings/Concatenation.java
+++ src/Strings/Concatenation.java
@@ -6,12 +6,13 @@ public class Concatenation {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the first Name ");
-        String firstName = sc.nextLine();
+        String firstName = sc.next();
         System.out.println("Enter the second Name ");
-        String secondName = sc.nextLine();
+        String secondName = sc.next();
 //        String firstName = "Hello";
 //        String secondName = "World";
         String fullNane = firstName + " " + secondName;
         System.out.println("fullName is  " + fullNane );
+
     }
 }
