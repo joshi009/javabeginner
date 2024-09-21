c97021362e8b05f54ef49ba2952d042b692bd851
kalyan joshi <joshi1307kashyap@gmail.com>
2024-09-21 14:01:04 +0530

    java

---
+++ src/Bit_Manipulation/Binary_Left_Shift.java
@@ -0,0 +1,11 @@
+package Bit_Manipulation;
+
+public class Binary_Left_Shift {
+    public static void main(String[] args) {
+        //formula = a*2^b
+        //  eg:  (5<<2)
+        //  0 0 0 1 0 1  (swap 2 zero forward from front side)
+        //  0 1 0 1 0 0  = (10100)
+        System.out.println((5<<2));
+    }
+}
