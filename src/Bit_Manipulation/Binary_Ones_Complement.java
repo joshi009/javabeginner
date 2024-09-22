b1571f967d9e6605bc0c9b4fed74b6b6e9a9c2f1
kalyan joshi <joshi1307kashyap@gmail.com>
2024-09-22 21:02:44 +0530

    java

---
+++ src/Bit_Manipulation/Binary_Ones_Complement.java
@@ -0,0 +1,12 @@
+package Bit_Manipulation;
+
+public class Binary_Ones_Complement {
+    public static void main(String[] args) {
+        //rules
+        //  ~0 = 1
+        //  ~1 = 0
+        // eg:  00000101
+        //      11111010
+        System.out.println((~5));
+    }
+}
