e933d9a22bfea0be045cc58921a2b30a12612e0d
kalyan joshi <joshi1307kashyap@gmail.com>
2024-09-24 19:27:56 +0530

    java

---
+++ src/Bit_Manipulation/Binary_Right_Shift.java
@@ -0,0 +1,11 @@
+package Bit_Manipulation;
+
+public class Binary_Right_Shift {
+    public static void main(String[] args) {
+        // rules
+        // (6>>1)
+        //  0 0 0 1 1 0     (swap 1 zero forward from back side)
+        //  0 0 0 0 1 1  = 3
+        System.out.println((6>>1));
+    }
+}
