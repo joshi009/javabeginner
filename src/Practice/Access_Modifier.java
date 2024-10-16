f144c308d4d1a1742bdda6a4917c679ac7816793
kalyan joshi <joshi1307kashyap@gmail.com>
2024-10-16 19:54:02 +0530

    java

---
+++ src/Practice/Access_Modifier.java
@@ -0,0 +1,10 @@
+package Practice;
+
+public class Access_Modifier {
+    public static void main(String[] args) {
+        int a = 24;
+        int b = 56;
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
