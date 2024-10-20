318d17a6de81195596ca4c4f4d56d79e8336f367
kalyan joshi <joshi1307kashyap@gmail.com>
2024-10-20 12:28:06 +0530

    java

---
+++ src/Practice/Modifier.java
@@ -0,0 +1,9 @@
+package Practice;
+import Practice.*;
+public class Modifier {
+    public static void main(String[] args) {
+        Access_Modifier as = new Access_Modifier();
+        int x = 56;
+        System.out.println(x);
+            }
+        }
