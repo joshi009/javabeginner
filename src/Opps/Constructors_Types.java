e9efac3bbddfdac3bf14bf7bc8440e6614a97070
kalyan joshi <joshi1307kashyap@gmail.com>
2024-10-23 22:01:33 +0530

    java

---
+++ src/Opps/Constructors_Types.java
@@ -0,0 +1,23 @@
+package Opps;
+
+public class Constructors_Types {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        Student s2 = new Student("Kalyan");
+        Student s3 = new Student(123);
+    }
+}
+     class Student {
+        String name;
+        int roll;
+        Student(){
+            System.out.println("constructor is called...");
+        }
+        Student(String name){
+            this.name = name;
+        }
+        Student (int roll){
+            this.roll= roll;
+        }
+    }
+
