0a564be6602c9470116daacda250dea59ff1c8c4
kalyan joshi <joshi1307kashyap@gmail.com>
2024-11-01 13:09:01 +0530

    java

---
+++ src/Opps/Method_Overloading.java
@@ -0,0 +1,26 @@
+package Opps;
+
+
+
+public class Method_Overloading {
+    public static void main(String[] args) {
+        calculator calc = new calculator();
+        System.out.println(calc.sum(1,2));
+        System.out.println(calc.sum((float)1.5,(float)2.5));
+        System.out.println(calc.sum(1,2,3));
+    }
+}
+
+class calculator{
+    int sum (int a , int b){
+        return a+b;
+    }
+    float sum (float a , float b){
+        return a+b;
+    }
+    int sum(int a, int b, int c){
+        return a+b+c;
+    }
+
+
+}
