4da73dc677670f1b9e1025d90dcb90bc44c7348a
kalyan joshi <joshi1307kashyap@gmail.com>
2024-12-15 18:42:48 +0530

    java

--- src/Mst/Calculator.java
+++ src/Mst/Calculator.java
@@ -1,10 +1,16 @@
 package Mst;
 
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) {
-        char ch = '-';
-        int a = 1;
-        int b = 2;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number :");
+        int a = sc.nextInt();
+        System.out.println("Enter the second number :");
+        int b = sc.nextInt();
+        System.out.println("Enter the operation + , - , * , / ,");
+        char ch = sc.next().charAt(0);
         switch (ch){
             case '+':
             System.out.println(a+b);
