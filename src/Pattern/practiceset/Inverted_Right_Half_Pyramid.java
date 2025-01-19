4666675bdcc3f217983a92abc653c61fca2ab730
kalyan joshi <joshi1307kashyap@gmail.com>
2025-01-19 09:51:40 +0530

    java

---
+++ src/Pattern/practiceset/Inverted_Right_Half_Pyramid.java
@@ -0,0 +1,26 @@
+package Pattern.practiceset;
+
+import java.util.Scanner;
+
+public class Inverted_Right_Half_Pyramid {
+        public static void main(String[] args)
+        {
+
+            // Number of rows
+            int x;
+            System.out.println("Enter the numbers of lines");
+            Scanner sc = new Scanner(System.in);
+            int N = sc.nextInt();
+
+
+            // Outer loop runs N times, once for each row
+            for (int i = 1; i <= N; i++) {
+                // Inner loop prints 'N - i + 1' stars
+                for (int j = 1; j <= N - i + 1; j++) {
+                    System.out.print("*" +" ");
+                }
+                // Move to the next line
+                System.out.println();
+            }
+        }
+    }
