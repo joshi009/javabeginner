3f64b93aabedb5a912390ce937d7f2a6df0504f9
kalyan joshi <joshi1307kashyap@gmail.com>
2025-02-08 22:09:50 +0530

    java

---
+++ src/ArrayList/Introduction.java
@@ -0,0 +1,10 @@
+package ArrayList;
+import java.util.ArrayList;
+
+public class Introduction {
+    public static void main(String[] args) {
+     ArrayList <Integer> list1  = new ArrayList<>();
+     ArrayList <Boolean> list2 = new ArrayList<>();
+     ArrayList <String> list3 = new ArrayList<>();
+    }
+}
