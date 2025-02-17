554dbedae199be9f84260e33ac81159a558bce98
kalyan joshi <joshi1307kashyap@gmail.com>
2025-02-17 20:21:54 +0530

    java

---
+++ src/ArrayList/Practice/String_ArrayList.java
@@ -0,0 +1,17 @@
+package ArrayList.Practice;
+
+import java.util.ArrayList;
+
+public class String_ArrayList {
+    public static void main(String[] args){
+
+        // create Arraylist
+        ArrayList<String> animals = new ArrayList<>();
+
+        // Add elements to LinkedList
+        animals.add("Dog");
+        animals.add("Cat");
+        animals.add("Cow");
+        System.out.println("ArrayList: " + animals);
+    }
+}
