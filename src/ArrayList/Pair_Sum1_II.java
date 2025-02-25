a043eb3634724dc3e3f07950fcb1fe054a65a0b4
kalyan joshi <joshi1307kashyap@gmail.com>
2025-02-25 23:01:06 +0530

    java

---
+++ src/ArrayList/Pair_Sum1_II.java
@@ -0,0 +1,37 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class Pair_Sum1_II {
+    public static Boolean pairSum1(ArrayList<Integer> list, int target){
+        int lp = 0;
+        int rp = list.size()-1;
+
+        while (lp != rp){
+            //case 1
+            if(list.get(lp) + list.get(rp) == target){
+                return true;
+            }
+            //case 2
+            if(list.get(lp) + list.get(rp) < target){
+                lp++;
+            }else {
+                //case 3
+                rp--;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+
+        int target = 10;
+        System.out.println(pairSum1(list,target));
+    }
+}
