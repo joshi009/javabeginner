d1a84371d43e4417099f29aafec52968fd3225f0
kalyan joshi <joshi1307kashyap@gmail.com>
2025-03-21 23:09:19 +0530

    java

---
+++ src/LinkedList1/Print_Linklist.java
@@ -0,0 +1,70 @@
+package LinkedList1;
+
+// Node class
+class Node1 {
+    int data;
+    Node next;
+
+    public Node1(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+// Linked List Class
+public class Print_Linklist {
+    private Node head;
+    private Node tail;
+
+    public Print_Linklist() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void print() {
+        if (head == null) {
+            System.out.println("Linked list is empty");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        Print_Linklist ll = new Print_Linklist();  // Corrected instance creation
+        ll.print();
+        ll.addFirst(1);
+        ll.print();
+        ll.addFirst(2);
+        ll.print();
+        ll.addLast(3);
+        ll.print();
+        ll.addLast(4);
+        ll.print();
+    }
+}
