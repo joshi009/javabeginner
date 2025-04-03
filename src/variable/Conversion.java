6436e049a4e2faf35035da951a5604fc13c1a44e
kalyan joshi <joshi1307kashyap@gmail.com>
2025-04-03 22:21:15 +0530

    java

--- src/variable/Conversion.java
+++ src/variable/Conversion.java
@@ -5,15 +5,16 @@ import java.util.Scanner;
 public class Conversion {
     public static void main(String args [] ) {
 
-    /*    long a = 4534;
+       long a = 26737;
         int b = 'a';
         System.out.println(a);
         System.out.println(b);
-      */
-        Scanner sc = new Scanner(System.in);
-//        int number = sc.nextFloat();
+
+//        Scanner sc = new Scanner(System.in);
+////        int number = sc.nextFloat();
+////        System.out.println(number);
+//        System.out.println("Enter the number : ");
+//        float number  = sc.nextInt();
 //        System.out.println(number);
-        float number  = sc.nextInt();
-        System.out.println(number);
     }
 }
