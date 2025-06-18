26d0b5e26f974c6930034d3200415c772ffa806a
kalyan joshi <joshi1307kashyap@gmail.com>
2025-06-18 21:41:08 +0530

    java

---
+++ src/Loop/For_Loop.java
@@ -0,0 +1,10 @@
+package Loop;
+
+public class For_Loop {
+    public static void main(String[] args) {
+        for (int i=1; i<=10; i++){
+            System.out.println("Kalyan");
+        }
+        System.out.println("print Kalyan 10X");
+    }
+}
