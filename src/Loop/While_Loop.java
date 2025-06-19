69d6f020fbf7f4382cf0c0ffebc81eb3b29ce195
kalyan joshi <joshi1307kashyap@gmail.com>
2025-06-19 22:13:33 +0530

    java

---
+++ src/Loop/While_Loop.java
@@ -0,0 +1,12 @@
+package Loop;
+
+public class While_Loop {
+    public static void main(String[] args) {
+        int Counter = 0;
+        while (Counter < 10 ){
+            System.out.println("Hello WOrld");
+            Counter++;
+        }
+        System.out.println("Print hello world 10 times");
+    }
+}
