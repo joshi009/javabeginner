3a36f933009bbc393cbe993bad49f70bb9d56a68
kalyan joshi <joshi1307kashyap@gmail.com>
2025-06-23 22:52:15 +0530

    java

---
+++ src/Loop/For_Loop_Star.java
@@ -0,0 +1,15 @@
+package Loop;
+
+public class For_Loop_Star {
+    public static void main(String[] args) {
+        for (int line = 1; line <= 4; line++){
+          System.out.println("* * * *");
+        }
+    }
+}
+//        int line = 1;
+//        while (line <= 4){
+//            System.out.println("* * * *");
+//            line++;
+//        }
+
