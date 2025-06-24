994945325c4fd084faaa301e6b53b9750c74f88e
kalyan joshi <joshi1307kashyap@gmail.com>
2025-06-24 22:02:16 +0530

    java

---
+++ src/Loop/While_Loop2.java
@@ -0,0 +1,11 @@
+package Loop;
+
+public class While_Loop2 {
+    public static void main(String[] args) {
+        int counter = 0 ;
+        while (counter <= 10){
+        System.out.print(counter + " ");
+        counter++;
+        }
+    }
+}
