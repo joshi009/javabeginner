54f6293b5615801a012b11d0a50aef3f20f37335
kalyan joshi <joshi1307kashyap@gmail.com>
2025-06-29 23:26:53 +0530

    java

--- src/variable/Promotion1.java
+++ src/variable/Promotion1.java
@@ -2,17 +2,17 @@ package variable;
 
 public class Promotion1 {
     public static void main(String args[] ){
-//        short  a = 5;
-//        byte b = 20;
-//        char c = 'k';
-//        byte bt =(byte) (a+b+c);
-//        System.out.println(bt);
+        short  a = 5;
+        byte b = 20;
+        char c = 'k';
+        byte bt =(byte) (a+b+c);
+        System.out.println(bt);
 
-        int a = 10;
-        float b = 20.35f;
-        long c =  25;
-        double d = 30;
-        double ans = a+b+c+d;
-        System.out.println(ans);
+//        int a = 10;
+//        float b = 20.35f;
+//        long c =  25;
+//        double d = 30;
+//        double ans = a+b+c+d;
+//        System.out.println(ans);
     }
 }
