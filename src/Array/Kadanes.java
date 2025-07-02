d6f615f4e472fe0ee53fef4453745d94af6dcfba
kalyan joshi <joshi1307kashyap@gmail.com>
2025-07-02 23:16:56 +0530

    java

--- src/Array/Kadanes.java
+++ src/Array/Kadanes.java
@@ -4,32 +4,6 @@ import java.util.*;
 
 
 public class Kadanes {
-    public static void maxSubarraySum(int numbers[] ){
-        int carrSum = 0;
-        int maxSum = Integer.MIN_VALUE;
-        int prefix[] = new int [numbers.length];
-
-        prefix[0] = numbers[0];
-        //calculate prefix array
-        for(int i=1; i<prefix.length; i++){
-            prefix[i] = prefix[i-1] + numbers[i];
-        }
-
-
-        for(int i=0; i<numbers.length; i++){
-            int start = i;
-            for(int j=i; j<numbers.length; j++){
-                int end = j;
-
-                carrSum = start == 0 ? prefix[end] : prefix[end] - prefix[start-1];
-
-                if(maxSum < carrSum){
-                    maxSum = carrSum;
-                }
-            }
-        }
-        System.out.println("max sum = " + maxSum);
-    }
     public static void kadanes(int numbers[] ){
         int ms = Integer.MIN_VALUE;
         int cs = 0;
@@ -46,6 +20,6 @@ public class Kadanes {
 
     public static void main(String[] args) {
         int numbers[] = {-2,-3,4,-1,-2,1,5,-3};
-        maxSubarraySum(numbers);
+        kadanes(numbers);
     }
 }
