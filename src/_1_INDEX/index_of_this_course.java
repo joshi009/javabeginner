f8bcf81cbe67d952b2a11bf2a9b0f41bd1f5dcb3
kalyan joshi <joshi1307kashyap@gmail.com>
2025-07-07 12:51:13 +0530

    java

---
+++ src/_1_INDEX/index_of_this_course.java
@@ -0,0 +1,33 @@
+package _1_INDEX;
+
+public class index_of_this_course {
+    public static void main(String[] args) {
+
+//        My playList has been meshed so i made Index for that...
+//        if you want to see my playList please go according to Index.
+
+//        INDEX...
+
+//        1.Flowchart and Pseudocode
+//        2. Variables and Data Types
+//        3. Operators
+//        4 Conditional Statements
+//        5 Loops
+//        6 Patterns
+//        7 Functions and Methods
+//        8 Patterns II
+//        9 Arrays
+//        10 Basic Sorting Algorithms
+//        11 2D Arrays
+//        12 Strings
+//        13 Bit Manipulation
+//        14 OOPS
+//        15 Recursion Basics
+//        16 Divide and Conquer
+//        17 Time and Space Complexity
+//        18 Backtracking
+//        19 ArrayLists
+//        20 LinkedList I
+
+    }
+}
