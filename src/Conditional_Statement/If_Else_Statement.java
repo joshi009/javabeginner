c0c4579ea47de8e9b67325e443f48e97ede7d194
kalyan joshi <joshi1307kashyap@gmail.com>
2025-07-10 22:52:50 +0530

    java

--- src/Conditional_Statement/If_Else_Statement.java
+++ src/Conditional_Statement/If_Else_Statement.java
@@ -1,12 +1,20 @@
 package Conditional_Statement;
 
 import java.util.Scanner;
+import java.util.*;
 
 public class If_Else_Statement {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-    }
+        System.out.println("Enter the age : ");
+        int age = sc.nextInt();
 
 
+        if (age >= 18) {
+            System.out.println("They can vote or Drive");
+        } else {
+            System.out.println("They are not adult");
+        }
+
     }
+}
