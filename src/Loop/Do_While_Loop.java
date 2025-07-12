8114050bbf2c1cd9e82e66052ef0cae930bf258e
kalyan joshi <joshi1307kashyap@gmail.com>
2025-07-12 15:11:20 +0530

    java

---
+++ src/Loop/Do_While_Loop.java
@@ -0,0 +1,11 @@
+package Loop;
+
+public class Do_While_Loop {
+    public static void main(String[] args) {
+        int counter = 1;
+        do{
+            System.out.println("Kalyan");
+            counter++;
+        }while ( counter <= 10);
+    }
+}
