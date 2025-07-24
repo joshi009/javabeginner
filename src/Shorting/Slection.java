e3ece65b470ddf21fe92fbd24836b6a334391773
kalyan joshi <joshi1307kashyap@gmail.com>
2025-07-24 22:14:54 +0530

    java

--- src/Shorting/Slection.java
+++ src/Shorting/Slection.java
@@ -1,13 +1,14 @@
 package Shorting;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Slection {
-    public static void slectionSort(int arr[] ) {
-        for(int i=0; i<arr.length-1; i++){
+    public static void slectionSort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
             int minPos = i;
-            for(int j=i+1; j< arr.length; j++){
-                if(arr[minPos] > arr[j]) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[minPos] > arr[j]) {
                     minPos = j;
                 }
             }
@@ -17,17 +18,16 @@ public class Slection {
             arr[i] = temp;
         }
     }
-//    public static void printArr(int arr[]){
-//        for(int i=0; i< arr.length; i++){
-//            System.out.print(arr[i] +" ");
-//        }
-//        System.out.println();
-//    }
+    public static void printArr(int arr[]){
+        for(int i=0; i< arr.length; i++){
+            System.out.print(arr[i] +" ");
+        }
+        System.out.println();
+    }
 
     public static void main(String[] args) {
-        int arr[] = {5,4,1,3,2};
-        slectionSort(arr);
-//        printArr(arr);
-        System.out.println(Arrays.toString(arr));
+    int arr[] = {5,4,1,3,2};
+    slectionSort(arr);
+    printArr(arr);
     }
 }
