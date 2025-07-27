068180c9e111b9d255ff125f4b53c5c2073fe85d
kalyan joshi <joshi1307kashyap@gmail.com>
2025-07-27 21:21:42 +0530

    java

---
+++ src/Conditional_Statement/Calculator.java
@@ -0,0 +1,42 @@
+package Conditional_Statement;
+
+import java.util.*;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter a number X : ");
+//        System.out.println("Enter a number Y : ");
+//        int X = sc.nextInt();
+//        int Y = sc.nextInt();
+//        char operator = sc.next().charAt(0);
+        HashMap<Integer,Integer>map=new HashMap<>();
+        map.put(1,2);
+        map.put(2,3);
+        map.put(3,4);
+        map.put(5,6);
+        map.put(53,253  );
+        System.out.println(map);
+
+
+
+
+//         switch (operator){
+//             case 1 :
+//                 System.out.println(X+Y);
+//                 break;
+//             case 4 :
+//                 System.out.println(X-Y);
+//                 break;
+//             case 2 :
+//                 System.out.println(X*Y);
+//                 break;
+//             case 3 :
+//                 System.out.println(X/Y);
+//                 break;
+//
+//         }
+    }
+}
