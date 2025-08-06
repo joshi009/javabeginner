7cebcddd7ad3343afdd779aa6db786a9b890b771
kalyan joshi <joshi1307kashyap@gmail.com>
2025-08-06 22:47:30 +0530

    java

---
+++ src/Conditional_Statement/Largest_2_Numbers.java
@@ -0,0 +1,13 @@
+package Conditional_Statement;
+
+public class Largest_2_Numbers {
+    public static void main(String[] args) {
+        int A = 5;
+        int B = 19;
+        if(A>B){
+            System.out.println("A is the largest numbers");
+        }else {
+            System.out.println("B is the largest numbers");
+        }
+    }
+}
