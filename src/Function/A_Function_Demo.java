69849bc2157861cde75218ec161024ca4ab7016d
kalyan joshi <joshi1307kashyap@gmail.com>
2025-08-14 22:46:18 +0530

    java

---
+++ src/Function/A_Function_Demo.java
@@ -0,0 +1,14 @@
+package Function;
+
+public class A_Function_Demo {
+    public static void printHelloWorld(){
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+
+    }
+    public static void main(String[] args) {
+
+        printHelloWorld();  //function call
+    }
+}
