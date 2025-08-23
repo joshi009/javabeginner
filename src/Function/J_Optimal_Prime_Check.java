a3d5f9961bed572e2a8d31235a78a8b9bd861ba9
kalyan joshi <joshi1307kashyap@gmail.com>
2025-08-23 23:41:25 +0530

    java

---
+++ src/Function/J_Optimal_Prime_Check.java
@@ -0,0 +1,33 @@
+package Function;
+
+import java.util.Scanner;
+
+public class J_Optimal_Prime_Check {
+    public static boolean isPrime(int n){
+        //corner case
+        if(n == 1 || n == 2){
+            return true;
+        }
+        boolean isPrime = true;
+        for(int i=2; i<Math.sqrt(n); i++){  // completely divide
+            if(n % i == 0){
+                isPrime = false;
+            }
+        }
+        return isPrime;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ente a number to check if it is prime: ");
+        int n = sc.nextInt();
+
+        if(isPrime(n)){
+            System.out.println(n+" is a Prime Number.");
+        }else {
+            System.out.println(n+" is a Not Prime Number.");
+        }
+//        System.out.println(isPrime(5));
+    }
+}
+
+
